7a9708aa922dea14cbcd5dfd3f14642ae964eec4
tomi <remioluwatomicephas@gmail.com>
2025-01-29 00:38:04 +0100

    handled cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
 
+    CORS(app, resources={
+        r"/*": {
+            "origins": "*", 
+            "methods": ["GET"], 
+            "allow_headers": ["Content-Type"]  ##only content type is needed for now.. limited this on purpose
+        }
+    })
+
+
     from .routes import main
     app.register_blueprint(main)
-    
+
     return app
